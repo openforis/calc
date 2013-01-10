e25aa010e3f3c5051a581e96c71391dda4b55fd5
Mino Togna <cosimo.togna@fao.org>
2013-01-10 11:50:27 +0100

    classes update after db refactor

---
+++ calc-core/src/main/java/org/openforis/calc/model/PlotSectionView.java
@@ -0,0 +1,11 @@
+package org.openforis.calc.model;
+
+
+/**
+ * @author G. Miceli
+ */
+public class PlotSectionView extends org.openforis.calc.persistence.jooq.tables.pojos.PlotSectionView {
+
+	private static final long serialVersionUID = 1L;
+
+}
