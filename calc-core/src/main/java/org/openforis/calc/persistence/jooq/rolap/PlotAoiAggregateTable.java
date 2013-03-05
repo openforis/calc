7ec96b69d17925a11b39fdf2b6f8d042a8fe5a74
Mino Togna <cosimo.togna@fao.org>
2013-03-05 12:11:36 +0100

    Plot Aoi aggregates

---
+++ calc-core/src/main/java/org/openforis/calc/persistence/jooq/rolap/PlotAoiAggregateTable.java
@@ -0,0 +1,49 @@
+package org.openforis.calc.persistence.jooq.rolap;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.jooq.Field;
+import org.openforis.calc.model.AoiHierarchyLevelMetadata;
+
+/**
+ * 
+ * @author M. Togna
+ *
+ */
+public class PlotAoiAggregateTable extends AoiAggregateTable<PlotAoiStratumAggregateTable> {
+
+	private static final long serialVersionUID = 1L;	
+
+	private List<Field<Integer>> aoiFields;
+
+	private AoiHierarchyLevelMetadata aoiHierarchyLevelMetadata;
+
+	PlotAoiAggregateTable(PlotAoiStratumAggregateTable factTable, AoiHierarchyLevelMetadata aoiHierarchyLevel) {
+		super(factTable, aoiHierarchyLevel.getAoiHierarchyLevelName());
+		this.aoiHierarchyLevelMetadata = aoiHierarchyLevel;
+		initFields();
+	}
+	
+	protected void initFields() {
+		PlotAoiStratumAggregateTable agg = getFactTable();
+		PlotFactTable fact = agg.getFactTable();
+//		createFixedDimensionField(fact.STRATUM_ID);
+		createFixedMeasureField(fact.EST_AREA);
+		aoiFields = createAoiFields(aoiHierarchyLevelMetadata.getAoiHierarchyLevelName());
+		initUserDefinedFields();
+	}
+
+	public List<Field<Integer>> getAoiFields() {
+		return Collections.unmodifiableList(aoiFields);
+	}
+	
+	@Override
+	public AoiHierarchyLevelMetadata getAoiHierarchyLevelMetadata() {
+		return aoiHierarchyLevelMetadata;
+	}
+	
+//	public String getAoiLevel() {
+//		return aoiLevel;
+//	}
+}
