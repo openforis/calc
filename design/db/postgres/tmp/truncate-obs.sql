9c0dd1f3f0560d3439169c9172b3aff43980d07a
minotogna <cosimo.togna@fao.org>
2013-04-10 00:19:13 +0200

    tmp script to truncate obs tables

---
+++ design/db/postgres/tmp/truncate-obs.sql
@@ -0,0 +1,8 @@
+truncate table calc.specimen_categorical_value CASCADE;
+truncate table calc.specimen_numeric_value CASCADE;
+truncate table calc.specimen CASCADE;
+
+truncate table calc.plot_section_aoi CASCADE;
+truncate table calc.plot_numeric_value CASCADE;
+truncate table calc.plot_categorical_value CASCADE;
+truncate table calc.plot_section CASCADE;
