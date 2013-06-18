a61937f92a75c4ba45a886f888ead1fe258d6d19
Gino Miceli <gino.miceli@fao.org>
2013-06-18 11:36:12 +0200

    Fixed workspace locking

---
+++ calc-core/src/main/java/org/openforis/calc/engine/SimpleLock.java
@@ -0,0 +1,36 @@
+package org.openforis.calc.engine;
+
+/**
+ * 
+ * @author G. Miceli
+ * @author M. Togna
+ *
+ */
+public final class SimpleLock {
+	private boolean locked;
+	
+	SimpleLock() {
+		this.locked = false;
+	}
+	
+	synchronized
+	public boolean isLocked() {
+		return locked;
+	}
+	
+	synchronized
+	public boolean tryLock() {
+		if ( locked ) {
+			return false;
+		}
+		this.locked = true;
+		return true;
+	}
+	
+	synchronized
+	public void unlock() {
+		if ( !locked ) {
+			throw new IllegalStateException("Already unlocked");
+		}
+	}
+}
