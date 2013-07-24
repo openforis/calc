cadc8e1bfed51606d8553262943dc845cf2778fb
Ricci Stefano <stefano.ricci@fao.org>
2013-07-24 09:26:20 +0200

    Code clean

--- calc-core/src/main/java/org/openforis/calc/persistence/CategoryDao.java
+++ calc-core/src/main/java/org/openforis/calc/persistence/CategoryDao.java
@@ -1,5 +1,7 @@
 package org.openforis.calc.persistence;
 
+import static org.openforis.calc.persistence.sql.Sql.quoteIdentifier;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -46,22 +48,22 @@ public class CategoryDao extends AbstractDao<Category> {
 				" FROM %s.%s";
 		
 		String sql = String.format(insertQueryTemplate, 
-				quote(outputSchema),
-				quote(CATEGORY_TABLE_NAME), 
-				quote(CATEGORY_VARIABLE_ID_COL_NAME),
-				quote(CATEGORY_ORIGINAL_ID_COL_NAME), 
-				quote(CATEGORY_CODE_COL_NAME),
-				quote(CATEGORY_NAME_COL_NAME),
-				quote(CATEGORY_DESCR_COL_NAME),
-				quote(CATEGORY_SORT_ORDER_COL_NAME),
+				quoteIdentifier(outputSchema),
+				quoteIdentifier(CATEGORY_TABLE_NAME), 
+				quoteIdentifier(CATEGORY_VARIABLE_ID_COL_NAME),
+				quoteIdentifier(CATEGORY_ORIGINAL_ID_COL_NAME), 
+				quoteIdentifier(CATEGORY_CODE_COL_NAME),
+				quoteIdentifier(CATEGORY_NAME_COL_NAME),
+				quoteIdentifier(CATEGORY_DESCR_COL_NAME),
+				quoteIdentifier(CATEGORY_SORT_ORDER_COL_NAME),
 				String.valueOf(variableId),
-				quote(codeTableIdColumnName),
-				quote(codeColumnName),
-				quote(codeTableLabelColumnName),
-				quote(codeTableDescriptionColumnName),
-				quote(codeTableIdColumnName),
-				quote(inputSchema),
-				quote(codeTableName)
+				quoteIdentifier(codeTableIdColumnName),
+				quoteIdentifier(codeColumnName),
+				quoteIdentifier(codeTableLabelColumnName),
+				quoteIdentifier(codeTableDescriptionColumnName),
+				quoteIdentifier(codeTableIdColumnName),
+				quoteIdentifier(inputSchema),
+				quoteIdentifier(codeTableName)
 		);
 
 		Connection c = getConnection();
@@ -73,10 +75,6 @@ public class CategoryDao extends AbstractDao<Category> {
 		}
 	}
 	
-	private String quote(String value) {
-		return value == null ? null : "\"" + value + "\"";
-	}
-
 	protected Connection getConnection() {
 		return DataSourceUtils.getConnection(dataSource);
 	}
