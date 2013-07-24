e57506f75a76429ea7963626c4a0ae98342dadfe
Ricci Stefano <stefano.ricci@fao.org>
2013-07-24 17:50:51 +0200

    Added "main" method for metadata sync

--- calc-core/src/main/java/org/openforis/calc/collect/CollectMetadataService.java
+++ calc-core/src/main/java/org/openforis/calc/collect/CollectMetadataService.java
@@ -6,19 +6,21 @@ import org.openforis.calc.engine.Workspace;
 import org.openforis.calc.engine.WorkspaceLockedException;
 import org.openforis.calc.engine.WorkspaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 /**
  * 
  * @author S. Ricci
- *
+ * 
  */
 @Component
 public class CollectMetadataService {
-	
+
 	@Autowired
 	private WorkspaceService workspaceManager;
-	@Autowired 
+	@Autowired
 	private TaskManager taskManager;
 
 	public Job startSync(int workspaceId) throws WorkspaceLockedException {
@@ -30,11 +32,21 @@ public class CollectMetadataService {
 
 	private Job createSyncJob(Workspace workspace) {
 		Job job = taskManager.createJob(workspace);
-		SyncMetadataTask syncMetadata = taskManager.createTask(SyncMetadataTask.class);
+		SyncMetadataTask syncMetadata = taskManager
+				.createTask(SyncMetadataTask.class);
 		job.addTask(syncMetadata);
-		SyncCategoriesTask syncCategories = taskManager.createTask(SyncCategoriesTask.class);
+		SyncCategoriesTask syncCategories = taskManager
+				.createTask(SyncCategoriesTask.class);
 		job.addTask(syncCategories);
 		return job;
+	}
+
+	public static void main(String[] args) throws WorkspaceLockedException {
+		@SuppressWarnings("resource")
+		ApplicationContext appContext = new ClassPathXmlApplicationContext(
+					"/applicationContext.xml");
+		CollectMetadataService service = appContext.getBean(CollectMetadataService.class);
+		service.startSync(1);
 	}
 
 }
