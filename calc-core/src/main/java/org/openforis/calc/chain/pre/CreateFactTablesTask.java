9d7a8a96d21feb59ef4e2be0cfb8c6ff58373a05
Alfonso Sanchez-Paus Diaz <Alfonso.SanchezPausDiaz@fao.org>
2013-08-07 10:07:21 +0200

    Fixes to SQL script

--- calc-core/src/main/java/org/openforis/calc/chain/pre/CreateFactTablesTask.java
+++ calc-core/src/main/java/org/openforis/calc/chain/pre/CreateFactTablesTask.java
@@ -60,7 +60,7 @@ public final class CreateFactTablesTask extends SqlTask {
 					for (Map<String, Object> row : listOfCategoriesForVar) {
 						psql()
 						.update( outputTable )
-						.set(categoryFKColumn, row.get(DIMENSION_TABLE_ID_COLUMN) )
+						.set(categoryFKColumn  + "=" + row.get(DIMENSION_TABLE_ID_COLUMN) )
 						.where( categoryFKColumn + " = ? ")
 						.execute(row.get( DIMENSION_TABLE_ORIGINAL_ID_COLUMN) );
 					}
