cb59f4da1ff99a3f13c8b468c7658d95d0c75774
Mino Togna <cosimo.togna@fao.org>
2013-09-10 17:44:41 +0200

    Implementation of "area" aggregate_type

--- calc-core/src/main/java/org/openforis/calc/metadata/VariableAggregate.java
+++ calc-core/src/main/java/org/openforis/calc/metadata/VariableAggregate.java
@@ -6,6 +6,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openforis.calc.common.NamedUserObject;
 
 /**
@@ -16,6 +17,18 @@ import org.openforis.calc.common.NamedUserObject;
 @Table(name = "variable_aggregate")
 public class VariableAggregate extends NamedUserObject {
 
+	public static enum AGGREGATE_TYPE {
+		MIN, MAX, SUM, MEAN, STDDEV, AREA, PER_UNIT_AREA;
+
+		public boolean equals(String other) {
+			return this.toString().toLowerCase().equals(other);
+		}
+
+		public String toString() {
+			return super.toString().toLowerCase();
+		}
+	}
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "variable_id")
 	private QuantitativeVariable variable;
@@ -23,8 +36,8 @@ public class VariableAggregate extends NamedUserObject {
 	@Column(name = "aggregate_column")
 	private String aggregateColumn;
 
-	@Column(name = "aggregate_function")
-	private String aggregateFunction;
+	@Column(name = "aggregate_type")
+	private String aggregateType;
 
 	@Column(name = "aggregate_formula")
 	private String aggregateFormula;
@@ -45,20 +58,16 @@ public class VariableAggregate extends NamedUserObject {
 		this.aggregateColumn = aggregateName;
 	}
 
-	/**
-	 * Mondrian function used for aggregating across categories 
-	 */
-	public String getAggregateFunction() {
-		return aggregateFunction;
+	public String getAggregateType() {
+		return aggregateType;
 	}
 
-	public void setAggregateFunction(String aggregateFunction) {
-		this.aggregateFunction = aggregateFunction;
+	public void setAggregateType(String aggregateType) {
+		this.aggregateType = aggregateType;
 	}
 
 	/**
-	 * Psql expression used for aggregating from single observations 
-	 * to stratum/AOI level 
+	 * Psql expression used for aggregating from single observations to stratum/AOI level
 	 */
 	public String getAggregateFormula() {
 		return aggregateFormula;
@@ -70,5 +79,25 @@ public class VariableAggregate extends NamedUserObject {
 
 	public void setAggregateColumn(String aggregateColumn) {
 		this.aggregateColumn = aggregateColumn;
+	}
+
+	/**
+	 * Mondrian function used for aggregating across categories
+	 */
+	public String getAggregateFunction() {
+		if ( AGGREGATE_TYPE.AREA.equals(aggregateType) ) {
+			return AGGREGATE_TYPE.SUM.toString();
+		} else {
+			return aggregateType;
+		}
+	}
+
+	/**
+	 * Returns true in case this aggregate is used in a virtual cube (e.g. CalculatedMember) and the column in the output table doesn't exist
+	 * 
+	 * @return
+	 */
+	public boolean isVirtual() {
+		return StringUtils.isBlank( getAggregateFormula() );
 	}
 }
