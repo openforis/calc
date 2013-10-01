bf37c63b5373cffc3804a96a79ee0a3a430faf85
Mino Togna <cosimo.togna@fao.org>
2013-10-01 12:14:53 +0200

    added font-awesome and navigation menu into section pages

--- calc-webapp/src/main/webapp/js/calc.js
+++ calc-webapp/src/main/webapp/js/calc.js
@@ -4,9 +4,12 @@
  *  @author Mino Togna
  */
 
+home = "home.html";
 $page = $("#page");
+$nav = $(".container ul.breadcrumb");
 
 loadPage = function(page) {
+	$page.hide();
 	$page.html("");
 	
 	$.ajax({
@@ -14,24 +17,38 @@ loadPage = function(page) {
 		  dataType: "html"
 	})
 	.done(function(response) {
+		if(page == home) {
+			$nav.hide();
+		} else {
+			$nav.fadeIn(500);
+		}
 		$page.html(response);
+		$page.fadeIn(500);
 	});
+	
 }; 
 
 $(document).ready(function() {
 	
-	$(".row-section .menu a").on("click", function(event) {
-		event.preventDefault();
-		
-		$href = $(this).attr("href");
-		console.log($href);
-	});
+//	$(".row-section .menu a").on("click", function(event) {
+//		event.preventDefault();
+//		
+//		$href = $(this).attr("href");
+//		console.log($href);
+//	});
+//	
+//	$(".home-label-link").on("click", function(event) {
+//		event.preventDefault();
+//		
+//		$href = $(this).attr("href");
+//		console.log($href);
+//	});
 	
-	$(".home-label-link").on("click", function(event) {
+	$nav.find("a").on("click", function(event) {
 		event.preventDefault();
 		
 		$href = $(this).attr("href");
-		console.log($href);
+		loadPage($href);
 	});
 	
 	loadPage("home.html");
