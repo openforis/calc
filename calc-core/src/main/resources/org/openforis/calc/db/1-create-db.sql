15368bb95d402ef1c0b2513304647189420eb00b
Ricci Stefano <stefano.ricci@fao.org>
2013-10-10 12:21:16 +0200

    Create db scripts

---
+++ calc-core/src/main/resources/org/openforis/calc/db/1-create-db.sql
@@ -0,0 +1,4 @@
+CREATE USER "calcdev" WITH PASSWORD 'calcdev';
+CREATE USER "calcuser" WITH PASSWORD 'calcuser';
+
+CREATE DATABASE calc ENCODING 'UTF8' OWNER calcdev;
---
+++ calc-core/src/main/resources/org/openforis/calc/db/2-create-schema.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA calc;
+
+ALTER SCHEMA calc OWNER TO calcdev;
