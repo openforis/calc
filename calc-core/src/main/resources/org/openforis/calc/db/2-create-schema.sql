7d316788a713dd646c621b53a6edde3d7d5c7299
Ricci Stefano <stefano.ricci@fao.org>
2013-10-10 14:39:07 +0200

    Fix database generation scripts

--- calc-core/src/main/resources/org/openforis/calc/db/2-create-schema.sql
+++
@@ -1,3 +0,0 @@
-CREATE SCHEMA calc;
-
-ALTER SCHEMA calc OWNER TO calcdev;
--- calc-core/src/main/resources/org/openforis/calc/db/init.sql
+++ calc-core/src/main/resources/org/openforis/calc/db/init.sql
@@ -2,3 +2,8 @@ CREATE USER "calcdev" WITH PASSWORD 'calcdev';
 CREATE USER "calcuser" WITH PASSWORD 'calcuser';
 
 CREATE DATABASE calc ENCODING 'UTF8' OWNER calcdev;
+
+\connect calc;
+
+CREATE SCHEMA calc;
+ALTER SCHEMA calc OWNER TO calcdev;
