90832bf9d16bd19f6efb70a924e431d09529eaf5
Mino Togna <cosimo.togna@fao.org>
2013-10-15 12:57:43 +0200

    initial calc setup

--- calc-core/src/main/java/test/ProcessingChainTest.java
+++ calc-core/src/main/java/test/ProcessingChainTest.java
@@ -2,7 +2,6 @@ package test;
 
 //import java.util.List;
 
-import org.junit.Test;
 import org.openforis.calc.chain.InvalidProcessingChainException;
 import org.openforis.calc.chain.ProcessingChain;
 import org.openforis.calc.chain.ProcessingChainDao;
@@ -13,8 +12,6 @@ import org.openforis.calc.engine.TaskManager;
 import org.openforis.calc.engine.Workspace;
 import org.openforis.calc.engine.WorkspaceLockedException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 /**
  * 
