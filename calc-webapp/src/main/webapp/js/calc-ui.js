050af9662be9b1f0ae47e06451114d1ea8d0410c
Ricci Stefano <stefano.ricci@fao.org>
2013-10-23 11:58:14 +0200

    Add documentation to UI.Form functions

--- calc-webapp/src/main/webapp/js/calc-ui.js
+++ calc-webapp/src/main/webapp/js/calc-ui.js
@@ -1,15 +1,32 @@
 var UI = {
 	
 	Form: {
+		/**
+		 * Returns the label associated to the specified field 
+		 * 
+		 * @param $field
+		 * @returns
+		 */
 		getFieldLabel: function($field) {
 			$formGroup = $field.closest('.form-group');
 			$labelEl = $formGroup.find('.control-label');
 			return $labelEl == null ? "": $labelEl.text();
 		},
+		/**
+		 * Remove all validation errors shown in the form
+		 * 
+		 * @param $form
+		 */
 		removeErrors: function ($form) {
 			$form.find('.form-group').removeClass('has-error');
 			$form.find('.form-control').tooltip('destroy');
 		},
+		/**
+		 * Update the validation errors in the form
+		 * 
+		 * @param $form
+		 * @param errors
+		 */
 		updateErrors: function($form, errors) {
 			UI.Form.removeErrors($form);
 			
@@ -21,6 +38,12 @@ var UI = {
 				}
 			});
 		},
+		/**
+		 * Show a success or failure message in a popup window
+		 * 
+		 * @param message
+		 * @param isSuccess
+		 */
 		showResultMessage: function(message, isSuccess){
 			// Add div if not defined
 			var feedbackDiv = $("#messageContainer");
@@ -38,12 +61,28 @@ var UI = {
 			// fade out
 			feedbackDiv.fadeIn( 400 ).delay( 2000 ).fadeOut( 800 );
 		},	
+		/**
+		 * Disable all input fields in a form
+		 * 
+		 * @param form
+		 */
 		disable: function(form){
 			form.find('input, textarea, button, select').prop('disabled', true);
 		},
+		/**
+		 * Enable all input fields in a form
+		 * 
+		 * @param form
+		 */
 		enable: function(form){
 			form.find('input, textarea, button, select').prop('disabled',false);
 		},
+		/**
+		 * Private function: create an error tooltip associated to a validation error
+		 * 
+		 * @param $field
+		 * @param error
+		 */
 		_createErrorTooltip: function($field, error) {
 			$formGroup = $field.closest('.form-group');
 			$formGroup.addClass('has-error');
@@ -60,6 +99,12 @@ var UI = {
 				template: '<div class="tooltip error"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
 			});
 		},
+		/**
+		 * Set the specified values into a form according to the field names
+		 * 
+		 * @param $form
+		 * @param $data
+		 */
 		setFieldValues: function($form, $data) {
 			$.each($data, function(fieldName, value) {
 				var $inputFields = $('[name='+fieldName+']', $form);
@@ -85,6 +130,13 @@ var UI = {
 				}
 		    });
 		},
+		/**
+		 * Returns the input type of a field.
+		 * If the field is not a "input" element, then returns the node name of the element.
+		 *  
+		 * @param inputField
+		 * @returns
+		 */
 		getInputType: function(inputField) {
 			var type = inputField.type;
 			if ( ! type ) {
@@ -93,17 +145,28 @@ var UI = {
 			}
 			return type;
 		},
+		/**
+		 * Sets all empty values in every input field inside the form
+		 * 
+		 * @param $form
+		 */
 		reset: function($form) {
 			//TODO support other field types: select, radio buttons, checkboxes...
 			$form.find("input[type=text], textarea").val("");
 			UI.Form.removeErrors($form);
 		}
 	},
+	/**
+	 * Shows a modal with static backdrop to avoid user interaction
+	 */
 	lock: function() {
 		$uiLockModal = $("#ui-lock-modal");
 		$uiLockModal.modal({keyboard:false, backdrop:"static"});
 		$('body').addClass('locked');
 	},
+	/**
+	 * Remove the lock modal
+	 */
 	unlock: function() {
 		$uiLockModal.modal('hide');
 		$uiLockModal.modal('removeBackdrop');
