77e3bed9a4ed29163b5469c33879f57ca63088c2
Mino Togna <cosimo.togna@fao.org>
2014-04-08 11:28:07 +0200

    work in progress removing hibernate

---
+++ calc-core/unused/old-hibernate/post/CreateAggregateTablesTask.java
@@ -0,0 +1,360 @@
+package org.openforis.calc.chain.post;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+
+import org.jooq.Condition;
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.SelectQuery;
+import org.jooq.impl.DSL;
+import org.jooq.impl.DynamicTable;
+import org.openforis.calc.engine.CalculationException;
+import org.openforis.calc.engine.Job;
+import org.openforis.calc.engine.Task;
+import org.openforis.calc.metadata.AoiLevel;
+import org.openforis.calc.metadata.QuantitativeVariable;
+import org.openforis.calc.metadata.SamplingDesign.TableJoin;
+import org.openforis.calc.psql.CreateTableStep.AsStep;
+import org.openforis.calc.psql.Psql;
+import org.openforis.calc.psql.Psql.Privilege;
+import org.openforis.calc.schema.AoiAggregateTable;
+import org.openforis.calc.schema.DataAoiTable;
+import org.openforis.calc.schema.DataTable;
+import org.openforis.calc.schema.EntityDataView;
+import org.openforis.calc.schema.ExpansionFactorTable;
+import org.openforis.calc.schema.DataSchema;
+import org.openforis.calc.schema.FactTable;
+import org.openforis.calc.schema.SamplingUnitAggregateTable;
+import org.openforis.calc.schema.Schemas;
+
+/**
+ * Creates and populates aggregate tables for sampling unit entities and descendants. One for each AOI level (at AOI/stratum level) is created.
+ * 
+ * @author M. Togna
+ */
+public final class CreateAggregateTablesTask extends Task {
+	
+//	private Entity entity;
+
+	public CreateAggregateTablesTask() {
+		super();
+//		this.entity = entity;
+	}
+
+	@Override
+	protected long countTotalItems() {
+		Schemas schemas = getJob().getSchemas();
+		List<FactTable> factTables = schemas.getDataSchema().getFactTables();
+		return factTables.size();
+	}
+	
+	protected void execute() throws Throwable {
+		DataSchema schema = getDataSchema();
+		
+		List<FactTable> factTables = schema.getFactTables();
+		for (FactTable factTable : factTables) {
+			// create fact table
+			createFactTable(factTable);
+		
+			// create plot aggregate table
+			SamplingUnitAggregateTable suAggregateTable = factTable.getSamplingUnitAggregateTable();
+			if( suAggregateTable != null ){
+				createSamplingUnitAggregateTable(suAggregateTable);
+			}
+			// create aggregation tables for each aoi level if there are
+			createAoiAggregateTables( factTable );
+			
+			incrementItemsProcessed();
+		}
+		
+	}
+
+	protected DataSchema getDataSchema() {
+		Job job = getJob();
+		Schemas schemas = job.getSchemas();
+		DataSchema schema = schemas.getDataSchema();
+		return schema;
+	}
+
+	private void createAoiAggregateTables(FactTable factTable) {
+		Collection<AoiAggregateTable> aggregateTables = factTable.getAoiAggregateTables();
+		for ( AoiAggregateTable aggTable : aggregateTables ) {
+			
+			DataTable sourceTable = aggTable.getSourceTable();
+			SelectQuery<Record> select = psql().selectQuery();
+			
+			select.addFrom( sourceTable );
+			
+//			select.addSelect( sourceTable.getDimensionIdFields() );
+			for (Field<Integer> dimField : sourceTable.getDimensionIdFields()) {
+				select.addSelect( DSL.coalesce(dimField,-1).as( dimField.getName() ) );
+			}
+			select.addGroupBy( sourceTable.getDimensionIdFields() );
+			
+			Collection<Field<Integer>> aoiIdFields = aggTable.getAoiIdFields();
+			for (Field<Integer> aoiField : aoiIdFields) {
+				Field<?> field = sourceTable.field(aoiField.getName());
+				select.addSelect( field );
+				select.addGroupBy( field );				
+			}
+
+			// join with expf table
+			AoiLevel aoiLevel = aggTable.getAoiLevel();
+			ExpansionFactorTable expfTable = getDataSchema().getExpansionFactorTable( aoiLevel );
+
+			Field<Integer> aoiField = sourceTable.getAoiIdField(aoiLevel);
+			Condition conditions = expfTable.AOI_ID.eq( aoiField );
+			if( getWorkspace().hasStratifiedSamplingDesign() ) {
+				
+				conditions = conditions.and( expfTable.STRATUM.eq( sourceTable.getStratumField() ) );
+
+				select.addSelect( sourceTable.getStratumField() );
+				select.addGroupBy( sourceTable.getStratumField() );
+			}
+			select.addJoin( expfTable, conditions );
+			select.addGroupBy( expfTable.EXPF );
+			
+			// add sum( quantity * expf )
+			// for now quantity fields. check if it needs to be done for each variable aggregate
+			for ( QuantitativeVariable var : sourceTable.getEntity().getDefaultProcessingChainOutputVariables() ) {
+				Field<BigDecimal> quantityField = sourceTable.getQuantityField(var);				
+				Field<BigDecimal> aggregateField = quantityField.mul( expfTable.EXPF ).sum();
+				
+				select.addSelect( aggregateField.as(quantityField.getName() ) );
+			}
+			
+			// aggregate count field (used by mondrian)
+			select.addSelect( DSL.count().as(aggTable.getAggregateFactCountField().getName()) );
+			
+			psql()
+				.dropTableIfExists(aggTable)
+				.execute();
+			
+			psql()
+				.createTable(aggTable)
+				.as(select)
+				.execute();
+		}
+		
+	}
+
+	private void createSamplingUnitAggregateTable( SamplingUnitAggregateTable suAggTable ) {
+//		SamplingUnitAggregateTable plotAgg = factTable.getPlotAggregateTable();
+		DataTable sourceTable = suAggTable.getSourceTable();
+		SelectQuery<Record> select = psql().selectQuery();
+		
+		select.addFrom( sourceTable );
+		
+		select.addSelect( sourceTable.getParentIdField() );
+		select.addGroupBy( sourceTable.getParentIdField() );
+//		select.addSelect( sourceTable.getDimensionIdFields() );
+		for (Field<Integer> dimField : sourceTable.getDimensionIdFields()) {
+			select.addSelect( DSL.coalesce(dimField,-1).as( dimField.getName() ) );
+		}
+		select.addGroupBy( sourceTable.getDimensionIdFields() );
+		select.addSelect( sourceTable.getAoiIdFields() );
+		select.addGroupBy( sourceTable.getAoiIdFields() );
+		select.addSelect( sourceTable.getStratumField() );
+		select.addGroupBy( sourceTable.getStratumField() );
+		
+		// for now quantity fields. check if it needs to be done for each variable aggregate
+		Field<BigDecimal> plotArea = ((FactTable)sourceTable) .getPlotAreaField();
+		if( plotArea == null ){
+			throw new CalculationException( "Plot area script has not been defined for entity " + sourceTable.getEntity().getName()+ ". Unable to aggregate this entity at plot level" );
+		}
+		for ( QuantitativeVariable var : sourceTable.getEntity().getDefaultProcessingChainOutputVariables() ) {
+			Field<BigDecimal> quantityField = sourceTable.getQuantityField(var);
+			
+			Field<BigDecimal> aggregateField = 
+				DSL.sum(
+					DSL.decode()
+					.when( plotArea.notEqual(BigDecimal.ZERO), quantityField.div(plotArea) )
+					.otherwise( BigDecimal.ZERO )
+				).as( quantityField.getName() );
+
+			select.addSelect( aggregateField );
+		}
+		
+		// drop table
+		psql()
+			.dropTableIfExists( suAggTable )
+			.execute();
+		
+		AsStep createTable = psql()
+			.createTable(suAggTable)
+			.as(select);
+		
+		createTable.execute();
+	}	
+	
+	
+	
+	private void createFactTable(FactTable factTable) {
+		EntityDataView dataTable = factTable.getEntityView();
+//		Entity entity = dataTable.getEntity();
+		
+		SelectQuery<?> select = new Psql().selectQuery(dataTable);
+		select.addSelect(dataTable.getIdField());
+		select.addSelect(dataTable.getParentIdField());
+		
+		// add dimensions to select
+		for (Field<Integer> field : factTable.getDimensionIdFields()) {
+			select.addSelect(dataTable.field(field));
+		}
+		for (Field<?> field : factTable.getCategoryValueFields() ) {
+			select.addSelect( dataTable.field(field) );
+		}
+
+		// add quantities to select
+		Collection<QuantitativeVariable> vars = factTable.getEntity().getOriginalQuantitativeVariables();
+		for (QuantitativeVariable var : vars) {
+			Field<BigDecimal> fld = dataTable.getQuantityField(var);
+			select.addSelect(fld);
+		}
+		vars = factTable.getEntity().getDefaultProcessingChainOutputVariables();
+		for (QuantitativeVariable var : vars) {
+			Field<BigDecimal> fld = dataTable.getQuantityField(var);
+			select.addSelect(fld);
+		}
+		
+		// add plot area to select
+		Field<BigDecimal> plotAreaField = factTable.getPlotAreaField();
+		if (plotAreaField != null) {
+			select.addSelect( dataTable.field(plotAreaField) );
+		}
+		
+		// add aoi ids to fact table if it's geo referenced
+		if( factTable.isGeoreferenced() ) {
+			DataAoiTable aoiTable = getJob().getInputSchema().getSamplingUnitAoiTable();
+			select.addSelect( aoiTable.getAoiIdFields() );
+			
+			Field<Long> joinField = ( dataTable.getEntity().isSamplingUnit() ) ? dataTable.getIdField() : dataTable.getParentIdField();
+			select.addJoin(aoiTable, joinField.eq(aoiTable.getIdField()) );
+		}
+		
+		// add stratum and cluster columns to fact table based on the sampling design
+		if( getWorkspace().getSamplingDesign().getTwoPhases() ){
+			
+			// add join in case of two phase sampling
+			DynamicTable<Record> phase1Table = factTable.getDataSchema().getPhase1Table();
+			TableJoin phase1Join = getWorkspace().getSamplingDesign().getPhase1Join();
+			Condition conditions = phase1Table.getJoinConditions( dataTable, phase1Join );
+			select.addJoin(phase1Table, conditions);
+			
+			// add stratum column
+			if( getWorkspace().hasStratifiedSamplingDesign() ) {
+				String stratumColumn = getWorkspace().getSamplingDesign().getStratumJoin().getColumn();
+				Field<Integer> stratumField = phase1Table.getIntegerField( stratumColumn ).cast(Integer.class).as( factTable.getStratumField().getName() ) ;
+				select.addSelect( stratumField );
+			}
+			// add cluster column
+			if( getWorkspace().hasClusterSamplingDesign() ) {
+				String clusterColumn = getWorkspace().getSamplingDesign().getClusterColumn().getColumn();
+				Field<String> clusterField = phase1Table.getVarcharField( clusterColumn ).as( factTable.getClusterField().getName() ) ;
+				select.addSelect( clusterField );
+			}
+		} else {
+			// one phase sampling
+			
+			if( getWorkspace().hasStratifiedSamplingDesign() ) {
+				// add stratum column
+				String stratumColumn = getWorkspace().getSamplingDesign().getStratumJoin().getColumn();
+				Field<Integer> stratumField = dataTable.field( stratumColumn ).cast(Integer.class).as( factTable.getStratumField().getName() ) ;
+				select.addSelect( stratumField );
+			}
+			
+			// add cluster column
+			if( getWorkspace().hasClusterSamplingDesign() ) {
+				String clusterColumn = getWorkspace().getSamplingDesign().getClusterColumn().getColumn();
+				Field<String> clusterField = dataTable.field( clusterColumn ).cast(String.class).as( factTable.getClusterField().getName() ) ;
+				select.addSelect( clusterField );
+			}
+		}
+			
+
+		
+		
+		// drop table
+		psql().dropTableIfExists( factTable ).execute();
+
+		// create table
+		AsStep createTable = psql().createTable( factTable ).as( select );
+		createTable.execute();
+
+		// Grant access to system user
+		psql().grant(Privilege.ALL).on(factTable).to(getSystemUser()).execute();
+	}
+	
+	
+	
+	@Override
+	public String getName() {
+		return String.format( "Create aggregate tables" );
+	}
+	
+//	@Override
+//	protected void oldExecute() throws Throwable {
+//		// TODO threshold
+//		OutputSchema outputSchema = getOutputSchema();
+//		Collection<AggregateTable> aggTables = outputSchema.getAggregateTables();
+//		ExpansionFactorTable expf = outputSchema.getExpansionFactorTable();
+//		for (AggregateTable aggTable : aggTables) {
+//			AoiLevel level = null;// aggTable.getAoiHierarchyLevel();
+//			NewFactTable f = (NewFactTable) aggTable.getSourceTable();
+//			Field<Integer> aoiId = f.getAoiIdField(level);
+//			Field<Integer> stratumId = f.getStratumField();
+//			Entity entity = aggTable.getEntity();
+//			Integer entityId = entity.getId();
+//			
+//			SelectQuery<?> select = new Psql().selectQuery(f);
+//			select.addSelect(f.getCategoryValueFields());
+//			select.addSelect(f.getDimensionIdFields());
+//			select.addSelect(stratumId);
+//			
+//			// Select AOI ID columns
+//			Collection<Field<Integer>> aoiIdFields = aggTable.getAoiIdFields();
+//			for (Field<Integer> aoiIdField : aoiIdFields) {
+//				select.addSelect(f.field(aoiIdField));
+//			}
+//			
+//			select.addGroupBy( select.getSelect() );
+//			select.addGroupBy( expf.EXPF );
+//			
+//			// Add aggregate columns
+//			List<VariableAggregate> variableAggregates = entity.getVariableAggregates();
+//			for (VariableAggregate varAgg : variableAggregates) {
+//				if( !varAgg.isVirtual() ){
+//					String formula = varAgg.getAggregateFormula();
+//					String aggCol = varAgg.getAggregateColumn();
+//					select.addSelect(DSL.field(formula).as(aggCol));
+//				}
+//			}
+//			
+//			//add aggregate fact count column
+//			select.addSelect(DSL.count().as(aggTable.getAggregateFactCountField().getName()));
+//			
+//			if ( isDebugMode() ) {
+//				psql()
+//					.dropTableIfExists(aggTable)
+//					.execute();
+//				
+//
+//			select.addJoin(expf, stratumId.eq(expf.STRATUM_ID)
+//				  .and(aoiId.eq(expf.AOI_ID))
+//				  .and(expf.ENTITY_ID.eq(entityId)));
+//
+//			psql().createTable(aggTable).as(select).execute();
+//
+//			// Grant access to system user
+//			psql()
+//				.grant(Privilege.ALL)
+//				.on(aggTable)
+//				.to(getSystemUser())
+//				.execute();
+//				
+//			}
+//		}
+//	}
+}
---
+++ calc-core/unused/old-hibernate/post/PublishRolapSchemaTask.java
@@ -0,0 +1,285 @@
+package org.openforis.calc.chain.post;
+
+import static org.openforis.calc.mondrian.Rolap.*;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.jooq.Field;
+import org.openforis.calc.engine.Task;
+import org.openforis.calc.engine.Workspace;
+import org.openforis.calc.mondrian.CalculatedMember;
+import org.openforis.calc.mondrian.DimensionUsage;
+import org.openforis.calc.mondrian.Hierarchy;
+import org.openforis.calc.mondrian.Hierarchy.Level;
+import org.openforis.calc.mondrian.Schema;
+import org.openforis.calc.mondrian.Schema.Cube;
+import org.openforis.calc.mondrian.Schema.Cube.Measure;
+import org.openforis.calc.mondrian.Schema.VirtualCube;
+import org.openforis.calc.mondrian.Schema.VirtualCube.CubeUsages;
+import org.openforis.calc.mondrian.Schema.VirtualCube.CubeUsages.CubeUsage;
+import org.openforis.calc.mondrian.Schema.VirtualCube.VirtualCubeDimension;
+import org.openforis.calc.mondrian.Schema.VirtualCube.VirtualCubeMeasure;
+import org.openforis.calc.mondrian.SharedDimension;
+import org.openforis.calc.mondrian.Table;
+import org.openforis.calc.mondrian.Table.AggName;
+import org.openforis.calc.mondrian.Table.AggName.AggForeignKey;
+import org.openforis.calc.mondrian.Table.AggName.AggLevel;
+import org.openforis.calc.mondrian.Table.AggName.AggMeasure;
+import org.openforis.calc.mondrian.View;
+import org.openforis.calc.saiku.Saiku;
+import org.openforis.calc.schema.AoiDimension;
+import org.openforis.calc.schema.CategoryDimension;
+import org.openforis.calc.schema.Dimension;
+import org.openforis.calc.schema.RolapSchema;
+import org.openforis.calc.schema.StratumDimension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 
+ * Task responsible for generating the rolap schema (using classes generated from mondrian 3.5.0 schema
+ * https://github.com/pentaho/mondrian/blob/3.5.0-R/lib/mondrian.xsd)
+ * 
+ * @author M. Togna
+ * 
+ */
+public class PublishRolapSchemaTask extends Task {
+
+	@Autowired
+	private Saiku saiku;
+	
+	public PublishRolapSchemaTask() {
+		super();
+	}
+
+	@Override
+	protected void execute() throws Throwable {
+		
+		Workspace workspace = getWorkspace();
+		RolapSchema rolapSchema = getRolapSchema();
+
+		// create schema
+		Schema schema = createSchema(rolapSchema.getName());
+
+		// create aoi dimensions
+		createAoiDimensions(rolapSchema, schema);
+
+		// create stratum dimension
+		createStratumDimension(rolapSchema, schema);
+
+		// create shared dimensions
+		createSharedDimensions(rolapSchema, schema);
+
+		// create cubes for each fact table
+		createCubes(rolapSchema, schema);
+
+		// create virtual cubes
+		createVirtualCubes(rolapSchema, schema);
+		
+		// publish schema
+		this.saiku.publishSchema(workspace, schema);
+	}
+
+	private void createCubes(RolapSchema rolapSchema, Schema schema) {
+		Collection<org.openforis.calc.schema.Cube> cubes = rolapSchema.getCubes();
+		for ( org.openforis.calc.schema.Cube rolapCube : cubes ) {
+
+			Cube cube = createCube(rolapCube.getName());
+			schema.getCube().add(cube);
+
+			// add table to cube
+			Table table = createTable(rolapCube.getSchema(), rolapCube.getTable());
+			cube.setTable(table);
+
+			// add agg names to table
+			createAggNames(rolapCube, table);
+
+			// add members to cube
+			createCubeMembers(rolapCube, cube);
+		}
+	}
+
+	private void createVirtualCubes(RolapSchema rolapSchema, Schema schema) {
+		for ( org.openforis.calc.schema.VirtualCube calcCube : rolapSchema.getVirtualCubes() ) {
+			VirtualCube virtualCube = createVirtualCube( calcCube.getName() );
+			schema.getVirtualCube().add( virtualCube );
+			
+			CubeUsages cubeUsages = new CubeUsages();
+			virtualCube.setCubeUsages( cubeUsages );
+			for( org.openforis.calc.schema.VirtualCube.CubeUsage u : calcCube.getCubeUsages() ) {
+				CubeUsage cubeUsage = createCubeUsage( u.getCubeName() );
+				cubeUsages.getCubeUsage().add( cubeUsage  );
+			}
+			
+			for (org.openforis.calc.schema.VirtualCube.VirtualCubeDimension d : calcCube.getVirtualCubeDimensions()) {
+				VirtualCubeDimension virtualDimension = createVirtualCubeDimension( d.getCubeName() , d.getName() );
+				virtualCube.getVirtualCubeDimension().add( virtualDimension );
+			}
+			
+			for ( org.openforis.calc.schema.VirtualCube.VirtualCubeMeasure m : calcCube.getVirtualCubeMeasures() ) {
+				VirtualCubeMeasure virtualMeasure = createVirtualCubeMeasure( m.getCubeName() , m.getName(), m.isVisible() );
+				virtualCube.getVirtualCubeMeasure().add( virtualMeasure  );
+			}
+			
+			for ( org.openforis.calc.schema.VirtualCube.CalculatedMember m : calcCube.getCalculatedMembers() ) {
+				CalculatedMember calculatedMember = createCalculatedMember( m.getDimension(), m.getName(), m.getCaption(), m.getFormula(), m.isVisible() );
+				virtualCube.getCalculatedMember().add( calculatedMember );
+			}
+			
+		}
+	}
+	
+	private void createCubeMembers(org.openforis.calc.schema.Cube rolapCube, Cube cube) {
+		// add stratum dimension usage
+		StratumDimension stratumDimension = rolapCube.getStratumDimension();
+		if( stratumDimension != null ) {
+			Field<Integer> stratumField =  rolapCube.getStratumField();
+			DimensionUsage stratumDimUsage = createDimensionUsage( stratumDimension.getName(), stratumField.getName() );
+			
+			cube.getDimensionUsageOrDimension().add(stratumDimUsage);
+		}
+
+		// add aoi dimension usages
+		Map<AoiDimension, Field<Integer>> aoiDimensionUsages = rolapCube.getAoiDimensionUsages();
+		for ( AoiDimension aoiDimension : aoiDimensionUsages.keySet() ) {
+			Field<Integer> field = aoiDimensionUsages.get(aoiDimension);
+			DimensionUsage dim = createDimensionUsage(aoiDimension.getName(), field.getName());
+			cube.getDimensionUsageOrDimension().add(dim);
+		}
+
+		// add dimension usages
+		Map<Dimension, Field<Integer>> dimensionUsages = rolapCube.getDimensionUsages();
+		for ( Dimension dimension : dimensionUsages.keySet() ) {
+			Field<Integer> field = dimensionUsages.get(dimension);
+
+			DimensionUsage dim = createDimensionUsage(dimension.getName(), field.getName());
+			cube.getDimensionUsageOrDimension().add(dim);
+		}
+
+		// add measures
+		Map<org.openforis.calc.schema.Measure, Field<BigDecimal>> measures = rolapCube.getMeasures();
+		for ( org.openforis.calc.schema.Measure measure : measures.keySet() ) {
+			Field<BigDecimal> field = measures.get(measure);
+			Measure m = createMeasure(measure.getName(), measure.getCaption(), field.getName(), measure.getAggregator(), DATA_TYPE_NUMERIC, NUMBER_FORMAT_STRING);
+
+			cube.getMeasure().add(m);
+		}
+	}
+
+	private void createAggNames(org.openforis.calc.schema.Cube rolapCube, Table table) {
+		int approxRowCnt = 100;
+		List<org.openforis.calc.schema.Cube.AggName> aggNames = rolapCube.getAggNames();
+		for ( org.openforis.calc.schema.Cube.AggName aggName : aggNames ) {
+			AggName aggTable = createAggregateName(aggName.getName(), approxRowCnt);
+			table.getAggTable().add(aggTable);
+
+			for ( org.openforis.calc.schema.Cube.AggForeignKey aggForeignKey : aggName.getAggForeignKeys() ) {
+				AggForeignKey aggFK = createAggForeignKey(aggForeignKey.getFactColumn(), aggForeignKey.getAggColumn());
+				aggTable.getAggForeignKey().add(aggFK);
+			}
+
+			for ( org.openforis.calc.schema.Cube.AggMeasure aggMeasure : aggName.getAggMeasures() ) {
+				AggMeasure aggM = createAggMeasure(aggMeasure.getName(), aggMeasure.getColumn());
+				aggTable.getAggMeasure().add(aggM);
+			}
+
+			for ( org.openforis.calc.schema.Cube.AggLevel aggLevel : aggName.getAggLevels() ) {
+				AggLevel aggL = createAggLevel(aggLevel.getHierarchy(), aggLevel.getName(), aggLevel.getColumn());
+				aggTable.getAggLevel().add(aggL);
+			}
+			approxRowCnt += 100;
+		}
+	}
+
+	private void createSharedDimensions( RolapSchema rolapSchema, Schema schema ) {
+		Collection<CategoryDimension> sharedDimensions = rolapSchema.getSharedDimensions();
+		for ( CategoryDimension categoryDimension : sharedDimensions ) {
+			SharedDimension dim = createDimension(categoryDimension);
+			schema.getDimension().add(dim);
+		}
+	}
+
+	private void createAoiDimensions(RolapSchema rolapSchema, Schema schema) {
+		List<AoiDimension> aoiDimensions = rolapSchema.getAoiDimensions();
+		for ( AoiDimension aoiDimension : aoiDimensions ) {
+			SharedDimension dim = createAoiDimension(aoiDimension);
+			schema.getDimension().add(dim);
+		}
+	}
+
+	private void createStratumDimension(RolapSchema rolapSchema, Schema schema) {
+		StratumDimension stratumDimension = rolapSchema.getStratumDimension();
+		if( stratumDimension != null ) {
+			
+			org.openforis.calc.schema.Hierarchy hierarchy = stratumDimension.getHierarchy();
+			org.openforis.calc.schema.Hierarchy.View view = hierarchy.getView();
+			SharedDimension dim = new SharedDimension();
+			
+			dim.setName(stratumDimension.getName());
+			dim.setType(DIMENSION_TYPE_STANDARD);
+			
+			Hierarchy h = new Hierarchy();
+			h.setName(hierarchy.getName());
+			h.setHasAll(true);
+			
+			List<org.openforis.calc.schema.Hierarchy.Level> levels = hierarchy.getLevels();
+			for ( org.openforis.calc.schema.Hierarchy.Level level : levels ) {
+				Level l = createLevel(level.getName(), level.getColumn(), level.getNameColumn(), level.getCaption() );
+				h.getLevel().add(l);
+			}
+
+			View v = createSqlView(view.getAlias(), view.getSql());
+			h.setView(v);
+
+			dim.getHierarchy().add(h);
+
+//			return dim;
+//			SharedDimension stratumSharedDimension = createDimension(stratumDimension);
+			schema.getDimension().add(dim);
+		}
+	}
+
+	private SharedDimension createDimension(Dimension dimension) {
+		org.openforis.calc.schema.Hierarchy hierarchy = dimension.getHierarchy();
+		org.openforis.calc.schema.Hierarchy.Table table = hierarchy.getTable();
+		org.openforis.calc.schema.Hierarchy.Level level = hierarchy.getLevels().get(0);
+
+		SharedDimension dim = createSharedDimension(dimension.getName(), table.getName(), table.getSchema(), level.getColumn(), level.getNameColumn(), level.getCaption() );
+		return dim;
+	}
+
+	private SharedDimension createAoiDimension(AoiDimension aoiDimension) {
+		org.openforis.calc.schema.Hierarchy hierarchy = aoiDimension.getHierarchy();
+		org.openforis.calc.schema.Hierarchy.View view = hierarchy.getView();
+
+		SharedDimension dim = new SharedDimension();
+		dim.setName(aoiDimension.getName());
+		dim.setType(DIMENSION_TYPE_STANDARD);
+		// ? dim.setCaption(hierarchy.get)
+
+		Hierarchy h = new Hierarchy();
+		h.setName(hierarchy.getName());
+		h.setHasAll(false);
+
+		List<org.openforis.calc.schema.Hierarchy.Level> levels = hierarchy.getLevels();
+		for ( org.openforis.calc.schema.Hierarchy.Level level : levels ) {
+			Level l = createLevel( level.getName(), level.getColumn(), level.getNameColumn() , level.getCaption() );
+			h.getLevel().add(l);
+		}
+
+		View v = createSqlView(view.getAlias(), view.getSql());
+		h.setView(v);
+
+		dim.getHierarchy().add(h);
+
+		return dim;
+	}
+	
+	@Override
+	public String getName() {
+		return "Publish schema to saiku repository";
+	}
+	
+}
