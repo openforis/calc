24bfc33f8530639d677d5f3d8593a20c8900306d
minotogna <mino.togna@gmail.com>
2019-06-25 15:56:23 +0200

    added workspaceId to init.R

--- calc-core/src/main/java/org/openforis/calc/chain/export/InitChainROutputScript.java
+++ calc-core/src/main/java/org/openforis/calc/chain/export/InitChainROutputScript.java
@@ -11,7 +11,7 @@ import org.openforis.calc.r.SetValue;
 import org.openforis.calc.schema.Schemas;
 
 /**
- * 
+ *
  * @author M. Togna
  *
  */
@@ -23,26 +23,31 @@ public class InitChainROutputScript extends ROutputScript {
 
 	private static RScript createScript(DBProperties dbProperties, Schemas schemas) {
 		RScript r = r();
-		
+
 		r.addScript( RScript.getCalcCommonScript() );
-		
+
 		// create driver
 		RVariable driver = r().variable("driver");
 		SetValue setDriver = r().setValue(driver, r().dbDriver("PostgreSQL"));
 		r.addScript( setDriver);
-		
+
 		// create db connection
 		DbConnect dbConnect = r().dbConnect( driver, dbProperties.getHost(), dbProperties.getDatabase(), dbProperties.getUser(), dbProperties.getPassword(), dbProperties.getPort() );
 		r.addScript( r().setValue( CONNECTION_VAR, dbConnect) );
-		
+
 		// set search path to current and public schemas
 		r.addScript(r().dbSendQuery( CONNECTION_VAR, new Psql().setDefaultSchemaSearchPath(schemas.getDataSchema(), new SchemaImpl("public"))));
-		
-		// set current dir 
+
+		r.addScript(r().comment("CALC global variables"));
+		// add workspaceId
+		r.addScript( r().rScript("workspaceId <- " + schemas.getWorkspace().getId() + ";"));
+
+
+		// set current dir
 //		RScript dirname = r().rScript( "dirname(sys.frame(1)$ofile)" );
 //		RScript setDirName = r().setValue( SCRIPT_DIR , dirname );
 //		r.addScript( setDirName );
-//		
+//
 //		Paste paste = r().paste( SCRIPT_DIR, r().variable("\"user\""), ".Platform$file.sep" );
 //		SetValue setUserScriptDir = r().setValue( USER_SCRIPT_DIR , paste );
 //		r.addScript(setUserScriptDir);
